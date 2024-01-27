8f235c7c3e6871b1f19ee4d28bcebeae9bc41171
tomik-z-cech <tkubancik@gmail.com>
2024-01-27 19:10:57 +0000

    Add favicon

--- ohmazing_components/urls.py
+++ ohmazing_components/urls.py
@@ -19,9 +19,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path('', include('landing.urls')),
+    path("favicon.ico", RedirectView.as_view(
+        url="/static/images/favicon.png")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
