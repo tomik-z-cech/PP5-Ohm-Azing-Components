58c341af61a916291f2091ad4fd708f19b4a4c95
tomik-z-cech <tkubancik@gmail.com>
2024-02-16 14:17:25 +0000

    Create "all_items" template

--- items/views.py
+++ items/views.py
@@ -1,22 +1,26 @@
 # Imports
 from django.shortcuts import render
 from django.views import generic
+from items.models import Category, Item
 
 
 class AllItemsView(generic.ListView):
     """
-    Class generates view of landing page
+    Class generates view of items page
     """
 
     template_name = "items/all_items.html"
 
     def get(self, request, *args, **kwargs):
-        """This method generates view of landing page"""
+        """This method generates view of items page"""
+        all_categories = Category.objects.all().order_by('category_name')
+        items = Item.objects.all().order_by('item_name')
         # Render template
         return render(
             request,
             self.template_name,
             {
-                "free_postage": 1
+                "all_categories": all_categories,
+                "items": items,
             },
         )
