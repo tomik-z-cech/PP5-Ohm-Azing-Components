61b3e6c97e67d243affe53d5744e52d5987aa3b2
Tomas Kubancik <tkubancik@gmail.com>
2024-02-24 21:46:57 +0000

    Add comments - profile_picture.js

--- static/js/profile_picture.js
+++ static/js/profile_picture.js
@@ -1,14 +1,23 @@
 $(document).ready(function() {
+    // Set variable of the div of profile picture file input
     let inputDiv = $("#id_profile_picture").closest("div");
+    // Hide the file input
     inputDiv.hide();
+    // When the pseudo input clicked, open actual file input
     $("#profile-picture-handler").click(function() {
         $("#id_profile_picture").click();
     });
+    // If the file picture changes
     $("#id_profile_picture").change(function() {
+        // Set variable of file details
         let file = this.files[0];
+        // If file uploaded correctly
         if (file) {
+            // Set file reader
             let reader = new FileReader();
+            // Once the reader loaded
             reader.onload = function(e) {
+                // Display the containers correctly
                 $("#profile-picture-preview").css("display", "block");
                 $("#initials").hide();
                 $("#profile-picture-preview").attr("src", e.target.result);
