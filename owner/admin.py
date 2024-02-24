160101a21778b98a188f999ffa6aef087723327d
tomik-z-cech <tkubancik@gmail.com>
2024-02-24 18:15:41 +0000

    Add Vouchers and Newsletter to owner admin

--- owner/admin.py
+++ owner/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from owner.models import Invoice, PostageSettings
+from owner.models import Invoice, PostageSettings, Voucher, Newsletter
 
 # Register your models here.
 @admin.register(Invoice)
@@ -13,3 +13,15 @@ class PostageSettingsAdmin(admin.ModelAdmin):
     """Class registers Postage Settings to Admin"""
 
     list_display = ("free_postage", "standard_delivery", "express_delivery")
+    
+@admin.register(Voucher)
+class VoucherAdmin(admin.ModelAdmin):
+    """Class registers Postage Settings to Admin"""
+
+    list_display = ("voucher_code", "start_date", "end_date", "status")
+    
+@admin.register(Newsletter)
+class PostageSettingsAdmin(admin.ModelAdmin):
+    """Class registers Postage Settings to Admin"""
+
+    list_display = ("newsletter_email",)
