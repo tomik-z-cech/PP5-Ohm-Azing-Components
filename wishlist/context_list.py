618624a73180d51eaeb62a656790f0b295726df1
Tomas Kubancik <tkubancik@gmail.com>
2024-02-27 21:36:38 +0000

    Add bubble to wishlist icon on header

--- wishlist/context_list.py
+++ wishlist/context_list.py
@@ -1,9 +1,17 @@
 def wishlist_content(request):
+    """
+    Method returns amount of items in wishlist
+    """
+    # If user is loged in
     if request.user.is_authenticated:
+        # Count item in wishlist of logged in user
         items_in_wishlist = len(request.user.userprofile.user_wishlist)
     else:
+        # If user is not loged in, amount = 0
         items_in_wishlist = 0
+    # Context dictionary
     wishlist_context = {
         "items_in_wishlist": items_in_wishlist,
     }
+    # Return context dictionary
     return wishlist_context
