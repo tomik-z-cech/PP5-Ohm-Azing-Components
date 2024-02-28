5cd5914a5f1670eebbb771144857d9c7484e2df5
tomik-z-cech <tkubancik@gmail.com>
2024-02-28 12:53:18 +0000

    Add logic for not doubling items in vault

--- vault/views.py
+++ vault/views.py
@@ -2,9 +2,26 @@ from django.shortcuts import redirect
 
 # Create your views here.
 def add_to_vault(request, item_pk):
-    # vault = []
+    """
+    Method adds item to vault in format :
+    [item_pk, size, value, quantity]
+    """
     vault = request.session.get('vault', [])
-    print(request.POST)
-    # vault.append(item_pk)
+    quantity = request.POST['quantity']
+    size = request.POST.get('size', 0)
+    value = request.POST.get('value', 0)
+    if len(vault) > 0:
+        for vault_item in vault:
+            append_new = True
+            if vault_item[0] == item_pk and vault_item[1] == size and vault_item[2] == value:
+                vault_item[3] = int(vault_item[3]) + int(quantity)
+                append_new = False
+            else:
+                item_to_add = [item_pk, size, value, quantity]
+        if append_new:
+            vault.append(item_to_add)
+    else:
+        item_to_add = [item_pk, size, value, quantity]
+        vault.append(item_to_add)
     request.session['vault'] = vault
     return redirect('item-detail', item_pk=item_pk)
