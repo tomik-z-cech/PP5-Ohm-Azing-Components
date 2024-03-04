e7e10ff30e5a21b05c497deb55e8f675bc6d625c
tomik-z-cech <tkubancik@gmail.com>
2024-03-04 19:38:10 +0000

    Adjust easter egg code and value

--- static/js/easter_egg.js
+++ static/js/easter_egg.js
@@ -6,12 +6,10 @@ let secretCode = '';
 $(document).keydown(function(event) {
         // Append the pressed key to the key sequence
         secretCode += event.key;
-        console.log(secretCode);
-
         // Check if the key sequence matches the specified string
         if (secretCode.includes('ilovetocode')) {
         // Show an alert window
-        alert('Great ! Enjoy 50% discount with code CI50 !');
+        alert('Great ! Enjoy 75% discount with code DJANGO75 !');
         // Reset the key sequence for the next input
         secretCode = '';
         }
